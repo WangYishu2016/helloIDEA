ad864612b00bf3b5fa410e020db95ccdeda1c0a8
WangYishu <17809290695@163.com>
2016-11-14 22:18:06 +0800

    Initial commit

---
+++ helloAPP/src/main/java/MathUnit.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Administrator on 2016/11/13.
+ */
+public class MathUnit {
+    public static void main(String[] args){
+        System.out.println(add(2,3));
+    }
+    public static int add(int a ,int b){
+        return a+b;
+    }
+}
